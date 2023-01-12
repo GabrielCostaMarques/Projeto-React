43b39ec9b60573d437d6f6861c66dfb31a2c18c1
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-01-12 14:44:55 -0300

    atualizando o componente campo texto e o app.js

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -1,11 +1,12 @@
 import './CampoTexto.css'
 
 const CampoTexto =(props)=>{
+
     return(
 
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input placeholder={`${props.placeholder}...`}/>
         </div>
     )
 }
