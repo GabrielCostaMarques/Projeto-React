107e63a8979a8a50c99b7cb0dd814f2f1506a3b6
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-01-17 10:15:29 -0300

    validação de campo

--- organo/src/componentes/CampoTexto/index.js
+++ organo/src/componentes/CampoTexto/index.js
@@ -1,12 +1,14 @@
 import './CampoTexto.css'
 
 const CampoTexto =(props)=>{
-
+    const aoDigitado = (evento)=>{
+        console.log(evento.target.value);
+    }
     return(
 
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={`${props.placeholder}...`}/>
+            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={`${props.placeholder}...`}/>
         </div>
     )
 }
