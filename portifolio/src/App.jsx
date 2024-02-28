00cf749f60237f4883cd476e15a2216e0775afbd
Gabriel Marques <gmaques@R11Travel.local>
2024-02-28 18:16:25 -0300

    Midle finished main content

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -1,11 +1,13 @@
 
 import Header from './components/Header'
 import './App.css'
+import MainBody from './components/Mainbody'
 
 function App() {
    return (
     <>
      <Header/>
+     <MainBody/>
     </>
   )
 }
