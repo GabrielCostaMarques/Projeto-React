530d4563e2df7ef7011d337417968f1763e2f362
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2024-05-27 14:00:39 -0300

    method post in db

--- requestHTTP/src/App.jsx
+++ requestHTTP/src/App.jsx
@@ -8,29 +8,70 @@ function App() {
 
   const [products, setProducts] = useState([])
 
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(URL);
       const data = await res.json();
       setProducts(data)
     }
-    console.log(products);
-  
+
     fetchData();
   }, []);
-  
-  console.log(products);
+
+  //add procuts
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product={
+      name,
+      price,
+    }
+
+    //fazendo um post 
+    const res=await fetch(URL,{
+      //temos de escolher nosso método
+      method:"POST",
+      //cabeçalho da requisição, qual tipo de conteúdo que estamos enviando
+      headers:{
+        "Content-Type":"application/json"
+      },
+
+      //definindo o corpo da requisição, transformando ele em JSON
+      body:JSON.stringify(product)
+    })
+
+
+    
+    // const addProduct
+
+  }
   return (
     <>
       <p>Lista de Produto</p>
       <ul>
-        {products.map((product)=>(
-
+        {products.map((product) => (
           <li key={product.id}>
             {product.name} - R$: {product.price}
           </li>
-
         ))}
+
+        <div className="add-product">
+          <form onSubmit={handleSubmit}>
+              <label>
+                Nome:
+                <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+
+                Price:
+                <input type="number" name='price' value={price} onChange={(e)=>setPrice(e.target.value)}/>
+              </label>
+
+              <input type="submit" name="Criar" value="Criar"/>
+          </form>
+        </div>
       </ul>
     </>
   )
